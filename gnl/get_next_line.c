53220808c81fd003e4c4bf0a25741096cc3720f2
Sacha Mallet <smallet@z1r3p3.le-101.fr>
2019-11-22 14:29:46 +0100

    Finish

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,85 +6,97 @@
 /*   By: smallet <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/05 10:29:19 by smallet      #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/15 14:03:58 by smallet     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/22 10:43:54 by smallet     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int     ft_scan(char *line)
+int		ft_scan(char *line)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (line[i])
-    {
-        if(line[i] == '\n')
-            return (1);
-        i++;
-    }
-    return (0);
+	i = 0;
+	while (line[i])
+	{
+		if (line[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
-char        *ft_init_check(int fd, char buf[BUFFER_SIZE + 1], char **tampon, char **line)
+char	*ft_init_check(int fd, char buf[BUFFER_SIZE + 1],
+		char **tampon, char **line)
 {
-    if ((fd < 0) || (BUFFER_SIZE <= 0) || (read(fd, &buf, 0) == -1))
-        return (NULL);
-    if (!(*tampon))
-        *tampon = ft_strdup("");
-    *line = ft_strdup("");
-    buf[0] = '\0';
-    return (*tampon);
+	if ((fd < 0) || (BUFFER_SIZE <= 0) || (read(fd, &buf, 0) == -1))
+		return (NULL);
+	if (!(*tampon))
+		if (!(*tampon = ft_strdup("")))
+			return (NULL);
+	if (!(*line = ft_strdup("")))
+		return (NULL);
+	buf[0] = '\0';
+	return (*tampon);
 }
 
-int     ft_end_read(char **temp, char **tampon, char buf[BUFFER_SIZE + 1])
+int		ft_end_read(char **temp, char **tampon, char buf[BUFFER_SIZE + 1])
 {
-    if (ft_scan(buf) == 1)
-    {
-        *temp = *tampon;
-        *tampon = ft_strdup(ft_memchr(buf, '\n'));
-        free(*temp);
-        return (1);
-    }
-    else
-    {
-        free(*tampon);
-        *tampon = NULL;
-        return (0);
-    }
+	if (ft_scan(buf) == 1)
+	{
+		*temp = *tampon;
+		if (!(*tampon = ft_strdup(ft_memchr(buf, '\n'))))
+			return (-1);
+		free(*temp);
+		return (1);
+	}
+	else
+	{
+		free(*tampon);
+		*tampon = NULL;
+		return (0);
+	}
 }
 
+int		ft_static_save(char **tampon, char **temp)
+{
+	if (ft_scan(*tampon) == 1)
+	{
+		*temp = *tampon;
+		if (!(*tampon = ft_strdup(ft_memchr(*tampon, '\n'))))
+			return (-1);
+		free(*temp);
+		return (1);
+	}
+	return (0);
+}
 
 int		get_next_line(int fd, char **line)
 {
-    static char *tampon;
-    char        *temp;
-    char        buf[BUFFER_SIZE + 1];
-    int         res;
+	static char	*tampon;
+	char		*temp;
+	char		buf[BUFFER_SIZE + 1];
+	int			res;
+	int			err;
 
-    if(!(ft_init_check(fd, buf, &tampon, &*line)))
-        return (-1);
-    if (ft_scan(tampon) == 1)
-    {
-        *line = ft_strjoin_free(*line, tampon);
-        temp = tampon;
-        tampon = ft_strdup(ft_memchr(tampon,'\n'));
-        free(temp);
-        return (1);
-    }
-    if (ft_scan(tampon) == 0)
-        *line = ft_strjoin_free(*line, tampon);
-    while (!(ft_scan(buf) == 1))
-    {
-        res = read(fd, &buf, BUFFER_SIZE);
-        buf[res] = '\0';
-        *line = ft_strjoin_free(*line, buf);
-        if (res != BUFFER_SIZE)
-            return(ft_end_read(&temp, &tampon, buf));
-    }  
-    temp = tampon;
-    tampon = ft_strdup(ft_memchr(buf, '\n'));
-    free(temp);
-    return (1);
+	if (!(ft_init_check(fd, buf, &tampon, line)) ||
+	!(*line = ft_strjoin_free(*line, tampon)))
+		return (-1);
+	if ((err = ft_static_save(&tampon, &temp)) != 0)
+		return (err);
+	while (!(ft_scan(buf) == 1))
+	{
+		res = read(fd, &buf, BUFFER_SIZE);
+		buf[res] = '\0';
+		if (!(*line = ft_strjoin_free(*line, buf)))
+			return (-1);
+		if (res != BUFFER_SIZE)
+			return (ft_end_read(&temp, &tampon, buf));
+	}
+	temp = tampon;
+	if (!(tampon = ft_strdup(ft_memchr(buf, '\n'))))
+		return (-1);
+	free(temp);
+	return (1);
 }
--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: smallet <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/15 11:11:14 by smallet      #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/15 14:05:29 by smallet     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/22 10:45:01 by smallet     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -24,9 +24,11 @@ int		ft_strlen(char *str);
 char	*ft_strdup(char *s1);
 char	*ft_memchr(char *s, int c);
 char	*ft_strjoin_free(char *s1, char *s2);
-int     ft_scan(char *line);
-char    *ft_init_check(int fd, char buf[BUFFER_SIZE + 1], char **tampon, char **line);
-int     ft_end_read(char **temp, char **tampon, char buf[BUFFER_SIZE + 1]);
+int		ft_scan(char *line);
+char	*ft_init_check(int fd, char buf[BUFFER_SIZE + 1],
+		char **tampon, char **line);
+int		ft_end_read(char **temp, char **tampon, char buf[BUFFER_SIZE + 1]);
+int		ft_static_save(char **tampon, char **temp);
 int		get_next_line(int fd, char **line);
 
 #endif
