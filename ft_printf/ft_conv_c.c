22c83cb1871b2477542eec99786d6cca03cc9cac
Rachadjamal <sacha.mallet1@gmail.com>
2020-01-10 22:04:20 +0100

    Width OK for all char

--- ft_printf/ft_conv_c.c
+++ ft_printf/ft_conv_c.c
@@ -42,20 +42,28 @@ void		ft_conv_pcent(t_printf *pf, va_list ap)
 
 void		ft_conv_ptr(t_printf *pf, va_list ap)
 {
-	unsigned long long int	ptr;
+	unsigned long int		ptr;
 	char					*res;
 	int						i;
+	int						width;
 
-	ptr = (unsigned long long int)va_arg(ap, void *);
+	ptr = (unsigned long int)va_arg(ap, void *);
 	if (!(res = ft_lltoa_base(ptr, "0123456789abcdef")))
 		return ;
 	i = 0;
+	width = pf->width - ft_strlen(res);
+	while (--width > 1 && pf->flagminus == 0)
+		pf->result += write(1, " ", 1);
 	pf->result += write(1, "0x", 2);
-	while (res[i])
+	if (pf->accuracy == 0 && ptr == 0)
 	{
-		pf->result += write(1, &res[i], 1);
-		i++;
+		free(res);
+		return ;
 	}
+	while (res[i])
+		pf->result += write(1, &res[i++], 1);
+	while (--width >= 1 && pf->flagminus == 1)
+		pf->result += write(1, " ", 1);
 	free(res);
 }
 
@@ -72,22 +80,40 @@ void		ft_conv_str(t_printf *pf, va_list ap)
 		if (!(str = ft_strdup("(null)")))
 			return ;
 	}
-	else if (!(str = ft_strdup(arg)))
-		return ;
-	i = 0;
+	else 
+		if (!(str = ft_strdup(arg)))
+			return ;
 	accu = pf->point == 0 ? (int)ft_strlen(str) : pf->accuracy;
+	i = accu == 0 ? pf->width : pf->width - ft_strlen(str);
+	while (--i >= 0 && pf->flagminus == 0)
+		pf->result += write(1, " ", 1);
+	i = 0;
 	while (str[i] && i < accu)
-	{
-		pf->result += write(1, &str[i], 1);
-		i++;
-	}
+		pf->result += write(1, &str[i++], 1);
+	i = accu == 0 ? pf->width : pf->width - ft_strlen(str);
+	while (--i >= 0 && pf->flagminus == 1)
+		pf->result += write(1, " ", 1);
 	free(str);
 }
 
 void		ft_conv_c(t_printf *pf, va_list ap)
 {
-	char c;
+	char	c;
+	int		i;
 
 	c = va_arg(ap, int);
+	i = 0;
+	if (pf->width > 1)
+	{
+		if (pf->flagminus == 0)
+			while (i++ < (pf->width - 1))
+				pf->result += write(1, " ", 1);
+	}
 	pf->result += write(1, &c, 1);
+	if (pf->width > 1)
+	{
+		if (pf->flagminus == 1)
+			while (i++ < (pf->width - 1))
+				pf->result += write(1, " ", 1);
+	}
 }
