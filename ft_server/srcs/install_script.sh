8bf01cdaafab539c185bbb4ae3f77dcf7218faff
Sacha Mallet <smallet@z1r3p3.le-101.fr>
2020-02-10 18:00:48 +0100

    wordpress install

--- ft_server/srcs/install_script.sh
+++ ft_server/srcs/install_script.sh
@@ -6,4 +6,5 @@ apt-get -y update && apt-get -y upgrade && apt-get -y install php7.3
 apt-get -y update && apt-get -y upgrade && apt-get -y install php7.3-fpm
 apt-get -y update && apt-get -y upgrade && apt-get -y install php7.3-mysql
 apt-get -y update && apt-get -y upgrade && apt-get -y install mariadb-server
+apt-get -y update && apt-get -y upgrade && apt-get -y install wordpress
 
