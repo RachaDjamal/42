ff77ae4f447d88197bdbcc6374a7e84c2999106f
Sacha Mallet <smallet@z1r3p3.le-101.fr>
2020-02-10 17:21:34 +0100

    phpmyadmin and wordpress

--- ft_server/srcs/install_script.sh
+++ ft_server/srcs/install_script.sh
@@ -4,5 +4,6 @@ apt-get -y update && apt-get -y upgrade && apt-get -y install nginx
 
 apt-get -y update && apt-get -y upgrade && apt-get -y install php7.3
 apt-get -y update && apt-get -y upgrade && apt-get -y install php7.3-fpm
-
+apt-get -y update && apt-get -y upgrade && apt-get -y install php7.3-mysql
+apt-get -y update && apt-get -y upgrade && apt-get -y install mariadb-server
 
