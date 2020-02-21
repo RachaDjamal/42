6e2ba60acad2dd53fd6cb261c8a802678802005f
Sacha Mallet <smallet@z1r3p3.le-101.fr>
2020-02-21 18:26:39 +0100

    ft_server OK

--- ft_server/make.sh
+++ ft_server/make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build -t server_image .
-docker run -itd -p 80:80 --name=server_container server_image:latest
+docker run -itd -p 80:80 -p 443:443 --name=server_container server_image:latest
 docker exec -it server_container bash
