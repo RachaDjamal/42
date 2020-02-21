6c44996ae0ac8b07cd952d96c2adb96bed90a20d
Sacha Mallet <smallet@z1r3p3.le-101.fr>
2020-02-21 14:11:33 +0100

    all good, without SSL

--- ft_server/srcs/launch_script.sh
+++ ft_server/srcs/launch_script.sh
@@ -5,12 +5,6 @@ service nginx start &
 service mysql start &
 service php7.3-fpm start 
 
-#echo "CREATE DATABASE wordpress;" | mysql -u root
-#mysql -u root -e "CREATE DATABASE wordpress;"
-#mysql -u root -e "CREATE USER 'user'@'localhost' IDENTIFIED BY 'password';"
-#mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'user'@'localhost';"
-#mysql -u root -e "FLUSH PRIVILEGES;"
-
 while [ true ]
 do
         true = true
